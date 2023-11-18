412cc9089750ccce5bfd95c9bc4f20af758cced0
ntt93 <1seeding3.3@gmail.com>
2023-11-18 10:19:32 +0700

    video css

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,4 +1,20 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = () => {
-  return <div className="homepage-container">home page component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+
+      <div className="homepage-content">
+        <div className="title-1">AAAAAAAAAAAAA</div>
+        <div className="title-2">bbb ccc ddd</div>
+        <div className="title-3">
+          <button>Get started</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HomePage;
